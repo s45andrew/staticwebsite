c9c4715734aad7e38cca8228b8a9e24450793084
s45andrew <s45andrew@googlemail.com>
2025-01-06 14:26:05 +0000

    iframe

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,10 @@ function App() {
       <div>
         <h1> Frantic Stats</h1>
         <a href="https://vite.dev" target="_blank">
-        <h2>data analytics</h2>
+        
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+        <h2>data analytics</h2>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
@@ -30,7 +31,24 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div class='aParent'>
+        <div id="pictures" class="column half">
+            <div id="window">
+                <div class="window-buttons">
+                    <button onclick="openNewPage('page1.html', '#2B0001')">Shares</button>
+                    <button onclick="openNewPage('page2.html', '#001E00')">News</button>
+                    <button onclick="openNewPage('page3.html', '#000328')">Sports</button>
+                    <button onclick="openNewPage('page4.html', '#6A4900')"> ToDo </button>
+                </div>
+                <div id="inner-window">
+                    <iframe id="inner-iframe" src=""></iframe>
+                </div>
+            </div>
+        </div>
+        </div>
     </>
+           
   )
 }
 
